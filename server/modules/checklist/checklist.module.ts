5bbf245c6725423a4e005d12e278c72bb51f7cb2
P1ne4pp1e <mabolong2006@outlook.com>
2026-09-19 23:26:25 +0800

    test: remove callback ingress probe after verification

--- server/modules/checklist/checklist.module.ts
+++ server/modules/checklist/checklist.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ChecklistController } from './checklist.controller';
-import { CallbackProbeController } from './callback-probe.controller';
 import { ChecklistService } from './checklist.service';
 
 @Module({
-  controllers: [ChecklistController, CallbackProbeController],
+  controllers: [ChecklistController],
   providers: [ChecklistService],
 })
 export class ChecklistModule {}
